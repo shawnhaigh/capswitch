17dd7404c7de5d79bf0357411b66c5abbe0dc4bc
Shawn Haigh <me@shawnhaigh.com>
2015-03-07 14:06:32 +0100

    master: added error checking

--- src/lib/capswitch.class.php
+++ src/lib/capswitch.class.php
@@ -21,23 +21,78 @@ class capswitch
     'USD' => 1.5897,
     'CHF' => 1.6135,
     'GBP' => 0.8031,
-    'JPY' => 164.5498,  // USD rate multiplied given JPY rate equals rate relative to EUR 
-    'CAD' => 1.6201     // GBP rate multiplied by given CAD rate equals rate relative to EUR
+    'JPY' => 164.5498,  // USD * JPY = JPY rate relative to EUR 
+    'CAD' => 1.6201     // GBP * CAD = CAD rate relative to EUR
     );
 
+  private $errors = array();
+
+  /*
+    Function to convert one currency to another.
+    Accepts three arguments:
+
+    from: (string) an ISO 4217 currency code
+    to: (string) an ISO 4217 currency code
+    amount: (float) amount to convert
+    returns a string
+  */
   public function convert($from, $to, $amount) 
   {
+    
+    /** Do some error checking
+     ***************************************************************************
+     */
+      
+      // Check if the source currency exisits in _exchrate
+      if ( !array_key_exists($from, $this->_exchrate) )
+      {
+        $this->errors[] = "ERROR: unable to retrieve SOURCE exchange rate" . PHP_EOL;
+      }
 
-    if ($to == "EUR") {
-      $value = $amount * ( 1 / $this->_exchrate[$from]) / $this->_exchrate[$to];
-    } else {
-      $value= $amount * $this->_exchrate[$to] / $this->_exchrate[$from];
-    }
+      // Check if the target currency exisits in _exchrate
+      if ( !array_key_exists($to, $this->_exchrate) )
+      {
+        $this->errors[] = "ERROR: unable to retrieve TARGET exchange rate". PHP_EOL;
+      }
 
-    $value = round($value, 4);
-    $value = number_format($value, 2, ".", ","); // english number format
-    $value = $value . ' ' . $to;
-    return $value;
+      // Check is amount is a number
+      if ( !is_numeric($amount) ) 
+      {
+        $this->errors[] = "ERROR: Ammount is not a number.". PHP_EOL;
+      }
+    
+      // If there are errors, return messages
+      if ( count($this->errors) ) 
+      {
+        $messages = implode($this->errors);
+        $this->errors = array(); // reset the array
+        return $messages;
+      }
+
+    /** Checks passed, on to main conversion
+     ***************************************************************************
+     */
+    
+      if ($to == "EUR")
+      {
+        // converting to EUR, find inverse of currency
+        $value = $amount * ( 1 / $this->_exchrate[$from]) / $this->_exchrate[$to];
+      } else {
+        // normal conversion calaulation
+        $value= $amount * $this->_exchrate[$to] / $this->_exchrate[$from];
+      }
+
+      // round up to 4 significant digits
+      $value = round($value, 4); 
+      
+      // english number format, truncate to two decimal places
+      $value = number_format($value, 2, ".", ",");
+      
+      // Append target Currentcy code
+      $value = $value . ' ' . $to . PHP_EOL; 
+
+      return $value;
+
   }
 
 }
--- src/public/index.php
+++ src/public/index.php
@@ -9,24 +9,27 @@
  */
 define('CAPSWITCH', TRUE);
 
-// Define a constant to the lib folder
+// define a constant to the lib folder
 define('__LIB__', dirname(dirname(__FILE__)).'/lib'); 
 
 // require the capswtich class
 require_once(__LIB__ . '/capswitch.class.php');
 
-// Instantiate the class
+// instantiate the class
 $c = new capswitch;
 
-echo $c->convert('EUR', 'USD', 100) . PHP_EOL;
-echo $c->convert('USD', 'EUR', 100) . PHP_EOL;
-echo $c->convert('EUR', 'CHF', 100) . PHP_EOL;
-echo $c->convert('EUR', 'GBP', 100) . PHP_EOL;
-echo $c->convert('USD', 'JPY', 100) . PHP_EOL;
-echo $c->convert('CHF', 'USD', 100) . PHP_EOL;
-echo $c->convert('GBP', 'CAD', 100) . PHP_EOL;
+echo $c->convert('EUR', 'USD', 100);
+echo $c->convert('USD', 'EUR', 100);
+echo $c->convert('EUR', 'CHF', 100);
+echo $c->convert('EUR', 'GBP', 100);
+echo $c->convert('USD', 'JPY', 100);
+echo $c->convert('CHF', 'USD', 100);
+echo $c->convert('GBP', 'CAD', 100);
 
-echo "---" . PHP_EOL;
+echo "Example of an invalid SOURCE currency" . PHP_EOL;
+echo $c->convert('GPL', 'USD', '100');
 
-echo $c->convert('PEP', 'USD', 100) . PHP_EOL;
+echo "Example of an invalid TARGET currency" . PHP_EOL;
+echo $c->convert('EUR', 'GPL', '100');
 
+ echo $c->convert('GBP', 'CAD', "F00");
